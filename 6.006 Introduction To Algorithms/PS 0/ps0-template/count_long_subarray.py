47aeab814aca06e2f32ee1cc7c23123a4dcfea1e
knn217 <nhanduyquan217@gmail.com>
2024-04-06 20:37:03 +0700

    PS0, only did 0-6 (Python), not learning Latex

--- 6.006 Introduction To Algorithms/PS 0/ps0-template/count_long_subarray.py
+++ 6.006 Introduction To Algorithms/PS 0/ps0-template/count_long_subarray.py
@@ -6,5 +6,18 @@ def count_long_subarray(A):
     count = 0
     ##################
     # YOUR CODE HERE #
+    longest = 0
+    length = 0
+    for i in range(len(A)): # iterate through A 1 time, O(N)
+        if i > 0 and A[i] > A[i-1]: # check if the elements are increasing
+            length += 1 # keep track of increase length
+        else:
+            length = 0 # if not increasing, reset the length
+
+        if length == longest: # every time length = longest, increase count by 1
+            count += 1
+        elif length > longest: # if length > longest, the previous count is meaningless, set count to 1 for the current longest
+            count = 1
+        longest = max(longest, length) # update longest each iter
     ##################
     return count
